d7b89e9dd0b90e369d461f703165de465404749b
EC2 Default User <ec2-user@ip-172-31-42-69.ec2.internal>
2021-09-18 10:39:11 +0000

    [Fix]4-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_131135) do
+ActiveRecord::Schema.define(version: 2021_09_17_225505) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -20,14 +20,6 @@ ActiveRecord::Schema.define(version: 2021_09_17_131135) do
   end
 
   create_table "post_comments", force: :cascade do |t|
-    t.text "comment"
-    t.integer "user_id"
-    t.integer "post_image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "post_commnets", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
     t.integer "post_image_id"
